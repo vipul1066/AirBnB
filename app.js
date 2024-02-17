55152c3c9e077a0626f0da6ff50255d02a18a3d4
vipul1066 <109271828+vipul1066@users.noreply.github.com>
2024-02-17 16:50:58 +0530

    Update app.js

--- app.js
+++ app.js
@@ -44,7 +44,7 @@ const store = MongoStore.create({
     crypto: {
         secret: process.env.SECRET,
     },
-    touchAfter: 24 * 3600
+    touchAfter: 24 * 3600,
 });
 
 store.on("error", () => {
