e9ce7a22dd614d06f4dc3402f6b824754f13a634
vipul1066 <109271828+vipul1066@users.noreply.github.com>
2024-02-17 16:28:49 +0530

    Update cloudConfig.js

--- cloudConfig.js
+++ cloudConfig.js
@@ -11,7 +11,7 @@ const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: {
       folder: 'wanderlust_DEV',
-      allowerdFormats: [ "jpg", "png", "jpeg"],
+      allowedFormats: [ "jpg", "png", "jpeg"],
     },
 });
 
